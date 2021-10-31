e6c737e085c62b5a967c03e6e630ef23f417703e
alex <sasha17@live.ru>
2021-10-31 22:06:00 +0300

    lighthouse for index, blog-index, students, koronavirus

--- pages/site.php
+++ pages/site.php
@@ -24,141 +24,141 @@ include DOCUMENT_ROOT . '/view/v3/head.php';
                 <tbody>
                 <tr>
                     <td>Главная</td>
-                    <td><img src="/images/site/html/index.png" alt="Главная" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/index.png" alt="Главная - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/index-mob.png" alt="Главная - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/index-pc.png" alt="Главная - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>О себе</td>
-                    <td><img src="/images/site/html/" alt="О себе" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/about.png" alt="О себе - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="О себе - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="О себе - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Портфолио</td>
-                    <td><img src="/images/site/html/" alt="Портфолио" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/portfolio" alt="Портфолио - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Портфолио - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Портфолио - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Путешествия</td>
-                    <td><img src="/images/site/html/travels.png" alt="Путешествия" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/travels.png" alt="Путешествия - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Путешествия - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Путешествия - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Свои проекты</td>
-                    <td><img src="/images/site/html/projects.png" alt="Свои проекты" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/projects.png" alt="Свои проекты - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Свои проекты - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Свои проекты - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Библиотека</td>
-                    <td><img src="/images/site/html/" alt="Библиотека" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/library.png" alt="Библиотека - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Библиотека - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Библиотека - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Статистика</td>
-                    <td><img src="/images/site/html/stats.png" alt="Статистика" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/stats.png" alt="Статистика - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Статистика - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Статистика - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Карта сайта</td>
-                    <td><img src="/images/site/html/sitemap.png" alt="Карта сайта" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/sitemap.png" alt="Карта сайта - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Карта сайта - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Карта сайта - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>О сайте</td>
-                    <td><img src="/images/site/html/" alt="О сайте" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/site.png" alt="О сайте - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="О сайте - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="О сайте - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Блог (главная)</td>
-                    <td><img src="/images/site/html/blog.png" alt="Блог (главная)" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog.png" alt="Блог (главная) - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/blog-index-mob.png" alt="Блог (главная) - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/blog-index-pc.png" alt="Блог (главная) - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Если бы люди не нуждались в еде</td>
-                    <td><img src="/images/site/html/blog-people-without-food.png" alt="Если бы люди не нуждались в еде" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-people-without-food.png" alt="Если бы люди не нуждались в еде - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Если бы люди не нуждались в еде - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Если бы люди не нуждались в еде - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Места для инвалидов в троллейбусе</td>
-                    <td><img src="/images/site/html/blog-seats-in-the-trolleybus.png" alt="Места для инвалидов в троллейбусе" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-seats-in-the-trolleybus.png" alt="Места для инвалидов в троллейбусе - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Места для инвалидов в троллейбусе - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Места для инвалидов в троллейбусе - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Наш ответ Marvel и DC</td>
-                    <td><img src="/images/site/html/blog-new-super-heroes.png" alt="Наш ответ Marvel и DC" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-new-super-heroes.png" alt="Наш ответ Marvel и DC - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Наш ответ Marvel и DC - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Наш ответ Marvel и DC - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Первая квартира</td>
-                    <td><img src="/images/site/html/blog-home-bay.png" alt="Первая квартира" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-home-bay.png" alt="Первая квартира - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Первая квартира - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Первая квартира - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Обновление сайта</td>
-                    <td><img src="/images/site/html/blog-site-update.png" alt="Обновление сайта" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-site-update.png" alt="Обновление сайта - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Обновление сайта - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Обновление сайта - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Моя черная полоса</td>
-                    <td><img src="/images/site/html/blog-black-lines.png" alt="Моя черная полоса" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-black-lines.png" alt="Моя черная полоса - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Моя черная полоса - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Моя черная полоса - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Новый личный беговой рекорд</td>
-                    <td><img src="/images/site/html/" alt="Новый личный беговой рекорд" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-run.png" alt="Новый личный беговой рекорд - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Новый личный беговой рекорд - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Новый личный беговой рекорд - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Мысли после просмотра 250 лучших фильмов всех времен</td>
-                    <td><img src="/images/site/html/" alt="Мысли после просмотра 250 лучших фильмов всех времен" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-movies-imdb.png" alt="Мысли после просмотра 250 лучших фильмов всех времен - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Мысли после просмотра 250 лучших фильмов всех времен - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Мысли после просмотра 250 лучших фильмов всех времен - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Обучение через подкасты</td>
-                    <td><img src="/images/site/html/" alt="Обучение через подкасты" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-podcasts.png" alt="Обучение через подкасты - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Обучение через подкасты - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Обучение через подкасты - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Треть сотни книг за 11 месяцев</td>
-                    <td><img src="/images/site/html/blog-books-2020.png" alt="Треть сотни книг за 11 месяцев" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-books-2020.png" alt="Треть сотни книг за 11 месяцев - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Треть сотни книг за 11 месяцев - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Треть сотни книг за 11 месяцев - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Планирование как степень достижения целей</td>
-                    <td><img src="/images/site/html/" alt="Планирование как степень достижения целей" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-planning.png" alt="Планирование как степень достижения целей - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Планирование как степень достижения целей - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/" alt="Планирование как степень достижения целей - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Мои мысли о коронавирусе</td>
-                    <td><img src="/images/site/html/blog-koronavirus.png" alt="Мои мысли о коронавирусе" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-koronavirus.png" alt="Мои мысли о коронавирусе - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/blog-koronavirus-mob.png" alt="Мои мысли о коронавирусе - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/blog-koronavirus-pc.png" alt="Мои мысли о коронавирусе - lighthouse" loading="lazy"></td>
                 </tr>
                 <tr>
                     <td>Напутствие будущим программистам</td>
-                    <td><img src="/images/site/html/blog-students.png" alt="Напутствие будущим программистам" loading="lazy"></td>
-                    <td></td>
-                    <td></td>
+                    <td><img src="/images/site/html/blog-students.png" alt="Напутствие будущим программистам - html валидатор" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/blog-students-mob.png" alt="Напутствие будущим программистам - lighthouse" loading="lazy"></td>
+                    <td><img src="/images/site/lighthouse/blog-students-pc.png" alt="Напутствие будущим программистам - lighthouse" loading="lazy"></td>
                 </tr>
                 </tbody>
         </table>
