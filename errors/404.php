7d14a91367f33c5a5d3fc2fddb4efd4896395faa
alexandr017 <sasha17@live.ru>
2022-12-29 22:15:41 +0300

    routes rewrite on php 8.2, add 404 template

---
+++ errors/404.php
@@ -0,0 +1,5 @@
+<?php
+http_response_code(404);
+echo 'Ошибка 404<br>';
+echo '<a href="/">Перейти на главную</a>';
+die();
--- routes.php
+++ routes.php
@@ -1,108 +1,40 @@
 <?php
-switch ($_SERVER['REQUEST_URI']) {
-    case '/' :
-        echo renderPage('pages/index.php');
-        break;
-    case '/alexandr-andreevich' :
-        echo renderPage('pages/about.php');
-        break;
-    case '/blog' :
-        echo renderPage('pages/blog/index.php');
-        break;
-    case '/blog/students' :
-        echo renderPage('pages/blog/2019/students.php');
-        break;
-    case '/blog/koronavirus' :
-        echo renderPage('pages/blog/2020/koronavirus.php');
-        break;
-    case '/blog/planning' :
-        echo renderPage('pages/blog/2020/planning.php');
-        break;
-    case '/blog/books-2020' :
-        echo renderPage('pages/blog/2020/books-2020.php');
-        break;
-    case '/blog/podcasts' :
-        echo renderPage('pages/blog/2021/podcasts.php');
-        break;
-    case '/blog/movies-imdb' :
-        echo renderPage('pages/blog/2021/movies-imdb.php');
-        break;
-    case '/blog/run' :
-        echo renderPage('pages/blog/2021/run.php');
-        break;
-    case '/blog/black-lines' :
-        echo renderPage('pages/blog/2021/black-lines.php');
-        break;
-    case '/blog/site-update' :
-        echo renderPage('pages/blog/2021/site-update.php');
-        break;
-    case '/blog/home-bay' :
-        echo renderPage('pages/blog/2021/home-bay.php');
-        break;
-    case '/blog/new-super-heroes' :
-        echo renderPage('pages/blog/2021/new-super-heroes.php');
-        break;
-    case '/blog/seats-in-the-trolleybus' :
-        echo renderPage('pages/blog/2021/seats-in-the-trolleybus.php');
-        break;
-    case '/blog/people-without-food' :
-        echo renderPage('pages/blog/2021/people-without-food.php');
-        break;
-    case '/blog/books-2021' :
-        echo renderPage('pages/blog/2021/books-2021.php');
-        break;
-    case '/blog/run-2018-2020' :
-        echo renderPage('pages/blog/2021/run-2018-2020.php');
-        break;
-    case '/blog/run-2021' :
-        echo renderPage('pages/blog/2021/run-2021.php');
-        break;
-    case '/blog/total-2021' :
-        echo renderPage('pages/blog/2021/total-2021.php');
-        break;
-    case '/blog/kinopoisk-250' :
-        echo renderPage('pages/blog/2021/kinopoisk-250.php');
-        break;
-    case '/blog/seo' :
-        echo renderPage('pages/blog/2022/seo.php');
-        break;
-    case '/blog/world-cup-2022' :
-        echo renderPage('pages/blog/2022/world-cup-2022.php');
-        break;
-    case '/blog/run-2022' :
-        echo renderPage('pages/blog/2022/run-2022.php');
-        break;
-    case '/blog/books-2022' :
-        echo renderPage('pages/blog/2022/books-2022.php');
-        break;
-    case '/portfolio' :
-        echo renderPage('pages/portfolio.php');
-        break;
-    case '/travels' :
-        echo renderPage('pages/travels.php');
-        break;
-    case '/projects' :
-        echo renderPage('pages/projects.php');
-        break;
-    case '/library' :
-    case '/library?show_type=short-list' :
-    case '/library?show_type=full-list' :
-        echo renderPage('pages/library.php');
-        break;
-    case '/stats' :
-        echo renderPage('pages/stats.php');
-        break;
-    case '/site' :
-        echo renderPage('pages/site.php');
-        break;
-    case '/sitemap' :
-        echo renderPage('pages/sitemap.php');
-        break;
-    case '/load-book' :
-        echo renderPage('ajax/load-book.php');
-        break;
 
+$template = match ($_SERVER['REQUEST_URI']) {
+    '/' => 'pages/index.php',
+    '/alexandr-andreevich' => 'pages/about.php',
+    '/blog' => 'pages/blog/index.php',
+    '/blog/students' => 'pages/blog/2019/students.php',
+    '/blog/koronavirus' => 'pages/blog/2020/koronavirus.php',
+    '/blog/planning' => 'pages/blog/2020/planning.php',
+    '/blog/books-2020' => 'pages/blog/2020/books-2020.php',
+    '/blog/podcasts' => 'pages/blog/2021/podcasts.php',
+    '/blog/movies-imdb' => 'pages/blog/2021/movies-imdb.php',
+    '/blog/run' => 'pages/blog/2021/run.php',
+    '/blog/black-lines' => 'pages/blog/2021/black-lines.php',
+    '/blog/site-update' => 'pages/blog/2021/site-update.php',
+    '/blog/home-bay' => 'pages/blog/2021/home-bay.php',
+    '/blog/new-super-heroes' => 'pages/blog/2021/new-super-heroes.php',
+    '/blog/seats-in-the-trolleybus' => 'pages/blog/2021/seats-in-the-trolleybus.php',
+    '/blog/people-without-food'=> 'pages/blog/2021/people-without-food.php',
+    '/blog/books-2021'=> 'pages/blog/2021/books-2021.php',
+    '/blog/run-2018-2020' => 'pages/blog/2021/run-2018-2020.php',
+    '/blog/run-2021' => 'pages/blog/2021/run-2021.php',
+    '/blog/total-2021' => 'pages/blog/2021/total-2021.php',
+    '/blog/kinopoisk-250' => 'pages/blog/2021/kinopoisk-250.php',
+    '/blog/seo' => 'pages/blog/2022/seo.php',
+    '/blog/world-cup-2022' => 'pages/blog/2022/world-cup-2022.php',
+    '/blog/run-2022' => 'pages/blog/2022/run-2022.php',
+    '/blog/books-2022' => 'pages/blog/2022/books-2022.php',
+    '/portfolio' => 'pages/portfolio.php',
+    '/travels' => 'pages/travels.php',
+    '/projects' => 'pages/projects.php',
+    '/library', '/library?show_type=short-list', '/library?show_type=full-list' => 'pages/library.php',
+    '/stats' => 'pages/stats.php',
+    '/site' => 'pages/site.php',
+    '/sitemap' => 'pages/sitemap.php',
+    '/load-book' =>'ajax/load-book.php',
+    default => 'errors/404.php'
+};
 
-    default:
-        renderPage('errors/404');
-}
+echo renderPage($template);
